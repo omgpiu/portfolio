25b758aa327b14b44f985c545a4fbcd7c61f12aa
omgpiu <dyba@List.ru>
2020-10-04 19:49:02 +0300

    Updates

---
+++ precache-manifest.a6750e26de10de9f5cbf8188955c4aac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4551f88b19ae6bc312fee3fd1b192e",
+    "url": "/porfolio/index.html"
+  },
+  {
+    "revision": "9e513d7cd3e632101d01",
+    "url": "/porfolio/static/css/main.e4e75286.chunk.css"
+  },
+  {
+    "revision": "dafce5288c8fb6f22fb2",
+    "url": "/porfolio/static/js/2.60344e1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/porfolio/static/js/2.60344e1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e513d7cd3e632101d01",
+    "url": "/porfolio/static/js/main.d1cde578.chunk.js"
+  },
+  {
+    "revision": "0a5a44b9d39ddbc4a4db",
+    "url": "/porfolio/static/js/runtime-main.e6904c30.js"
+  },
+  {
+    "revision": "b305ce629d5d0fab693a52971809824c",
+    "url": "/porfolio/static/media/mini_socialnetwork.b305ce62.jpg"
+  },
+  {
+    "revision": "c5cc100a7fa0aa1c99dfd0fa60ca032d",
+    "url": "/porfolio/static/media/newcounter.c5cc100a.png"
+  },
+  {
+    "revision": "c9dda44ca498266911feec3647856a28",
+    "url": "/porfolio/static/media/newtodo.c9dda44c.png"
+  }
+]);
