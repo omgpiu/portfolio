dfa0f616d65f8ab6de56092e47a90f7a37282ab6
omgpiu <dyba@list.ru>
2020-12-18 09:51:27 +0300

    Header name,map

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -3,11 +3,12 @@ import st from './Header.module.css'
 import Nav from '../nav/Nav';
 
 
-
-const Header =()=>{
+const Header = () => {
     return <div className={st.header}>
-
-        <Nav/>
+        <div className={st.item}>ALEX DUBROVSKII</div>
+        <div className={st.item}>
+            <Nav/>
+        </div>
 
 
     </div>
--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -3,13 +3,18 @@ import st from './Nav.module.css';
 
 
 const Nav = () => {
-    return <div className={st.nav}>
-        <a href="https://www.google.com/">HOME</a>
-        <a href="https://www.google.com/">ABOUT</a>
-        <a href="https://www.google.com/">SERVICES</a>
-        <a href="https://www.google.com/">WORKS</a>
-        <a href="https://www.google.com/">BLOG</a>
-        <a href="https://www.google.com/">CONTACT</a>
+    const navArray = ['HOME', 'ABOUT', 'SERVICES', 'WORKS', 'BLOG', 'CONTACT']
+    return <div>
+        {/*<a href="https://www.google.com/">HOME</a>*/}
+        {/*<a href="https://www.google.com/">ABOUT</a>*/}
+        {/*<a href="https://www.google.com/">SERVICES</a>*/}
+        {/*<a href="https://www.google.com/">WORKS</a>*/}
+        {/*<a href="https://www.google.com/">BLOG</a>*/}
+        {/*<a href="https://www.google.com/">CONTACT</a>*/}
+        <ul className={st.nav}>
+            {navArray.map(el =>
+                <ol>{el}</ol>)}
+        </ul>
 
 
     </div>;
