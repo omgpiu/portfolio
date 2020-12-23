8f5714cc830254b6833ec53a0fc452f5a5ff8735
omgpiu <dyba@list.ru>
2020-12-23 16:03:02 +0300

    progress circle done

--- src/common/progress/progressbar.tsx
+++ src/common/progress/progressbar.tsx
@@ -2,19 +2,15 @@ import React from 'react';
 import st from './progressbar.module.css'
 
 const ProgressCircle = () => {
-    return <div className={st.body}>
-        <div className={st.box}>
-            <div className={st.percent}>
-                <svg>
-                    <circle cx={70} cy={70} r={70}></circle>
-                    <circle cx={70} cy={70} r={70}></circle>
-                </svg>
-                <div className={st.number}>
-                    <span className={st.number}>87%</span>
-                </div>
-            </div>
-            <span className={st.progress}>Progress</span>
+    return <div className={st.box}>
+        <div className={st.percent}>
+            <svg>
+                <circle cx={70} cy={70} r={70}></circle>
+                <circle cx={70} cy={70} r={70}></circle>
+            </svg>
+            <span className={st.number}>87%</span>
         </div>
+        <span className={st.progress}>Progress</span>
     </div>
 }
 export default ProgressCircle;
