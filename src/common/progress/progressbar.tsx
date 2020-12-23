2f1dd8a5d45ec1e4cdcee812a83b8acf0aebd252
omgpiu <dyba@list.ru>
2020-12-23 15:30:06 +0300

    progress circle done

--- src/common/progress/progressbar.tsx
+++ src/common/progress/progressbar.tsx
@@ -9,8 +9,8 @@ const ProgressCircle = () => {
                     <circle cx={70} cy={70} r={70}></circle>
                     <circle cx={70} cy={70} r={70}></circle>
                 </svg>
-                <div className={st.percent}>
-                    <span>87<span>%</span></span>
+                <div className={st.number}>
+                    <span className={st.number}>87%</span>
                 </div>
             </div>
             <span className={st.progress}>Progress</span>
