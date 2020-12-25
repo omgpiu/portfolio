018cc0b26945762ca14d3af6c87bd89fc8f2d5c4
omgpiu <dyba@list.ru>
2020-12-25 11:50:32 +0300

    progress circle done

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,8 @@ import Works from './works/Works';
 import Contact from './contact/Contact';
 import Footer from './footer/Footer';
 import About from "./about/About";
-import Services from "./services/Services";
 import Education from "./education/Education";
 import st from './App.module.css'
-
-// window.onscroll = function() {
-//     checkMarginToTop();
-// };
-//
-// var nav = document.getElementById("nav");
-//
-// var sticky = nav.offsetTop;
-//
-// function checkMarginToTop() {
-//     if (window.pageYOffset > sticky) {
-//         nav.classList.add("sticky");
-//     } else {
-//         nav.classList.remove("sticky");
-//     }
-// }
 
 
 const App = () => {
@@ -47,13 +30,14 @@ const App = () => {
                 <Main/>
                 <About/>
 
-                <Services/>
+                {/*<Services/>*/}
 
                 <Skills/>
                 <Education/>
                 <Works/>
                 <Contact/>
                 <Footer/>
+
             </>
         );
     }
